0ea2ecebf5268c446b7e8a8fc9a0c7811904d246
Maxym Berezynets <halustyan@ukr.net>
2020-05-26 14:51:58 +0300

    new

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,40 @@
-"use strict"; // Не ппонял, для чего эта строка
+"use strict";
 
 var gulp = require("gulp");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
 var rename = require("gulp-rename");
-var server = require("browser-sync").create(); // Команда запуска локального сервера
-
-var sass = require("gulp-less");
+var server = require("browser-sync").create();
+var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var csso = require("gulp-csso");
-
 var imagemin = require("gulp-imagemin");
 var svgstore = require("gulp-svgstore");
-
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
-
 var htmlmin = require("gulp-htmlmin");
-
 var uglify = require('gulp-uglify');
-
 var del = require("del");
+var clean = require('gulp-clean');
 
-// Задача копирования файлов и папок в папку build
+gulp.task('clean', function () {
+  return gulp.src('source/less/*.css', {read: false})
+      .pipe(clean());
+});
+
 gulp.task("copy", function () {
   return gulp
     .src([
       "source/fonts/**/*.{woff,woff2}",
-      "source/img/**/*.webp",  // картинки остальных форматов положу при оптимизации
-      "source/js/*.min.js"     // копирую только минифицированые js-файлы
+      "source/img/**/*.webp",
+      "source/js/*.min.js"
     ], {
       base: "source"
     })
     .pipe(gulp.dest("build"));
 });
 
-// Задача очистки папки build
 gulp.task("clear", function () {
   return del("build");
 });
@@ -45,94 +43,103 @@ gulp.task("html", function () {
   return gulp
     .src("source/*.html")
     .pipe(posthtml([
-      include()      // добавляю возможность в html использовать тег-директиву <include src="file.*"></include>
+      include()
     ]))
-    .pipe(htmlmin({  // минифицирую html-файл
+    .pipe(htmlmin({
       collapseWhitespace: true,
       removeComments: true
     }))
     .pipe(gulp.dest("build"));
 });
 
-// задача компиляции файла style.scss в файл style.min.css
 gulp.task("css", function () {
   return gulp
-    .src("source/less/style.less") // Найти файл style.scss
-    .pipe(plumber())               // Проверяет корректность написанного кода и в консоли показывает ошибк
-    .pipe(sourcemap.init())        // Иницизирует работу карты кода, фиксируя состояние препроцессорного кода
-    .pipe(sass())                  // Компилирует sass-файлы в css-файл
-    .pipe(postcss([                // Своего рода парссер css-кода со своими методами и функциями
-      autoprefixer()               // Добавляет префиксы в css-файл
+    .src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
     ]))
-    .pipe(csso())                  // Минифицирует css-файл
-    .pipe(rename("style.min.css"))
-    .pipe(sourcemap.write("."))    // В конечном состоянии css-файла происходит перелиновка связей с scss
+    .pipe(csso())
     .pipe(gulp.dest("build/css"))
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("css2", function () {
+  return gulp
+    .src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
 });
 
-// Задача минимизации js
+
 gulp.task("uglify", function () {
   return gulp
-    .src(["source/js/*.js", "!*.min.js"]) // выбираю не минифицированные файлы js
+    .src(["source/js/*.js", "!*.min.js"])
     .pipe(uglify())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest("build/js")) // Поместить результируюий css-файл в указанную папку
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest("build/js"))
 });
 
-// Задача оптимизации изображений
 gulp.task("images", function () {
   return gulp
-    .src(["source/img/**/*.{jpg,png,svg}", "!sprite.svg"]) // ищу изображения.
-                            // sprite.svg исключаю, т.к. ранее собрал его вручную
+    .src(["source/img/**/*.{jpg,png,svg}", "!sprite.svg"])
+
     .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}), // оптимизируем png
-      // оптимизируем jpeg.
-      //imagemin.jpegtran({progressive: true}),   // --- не сработал
+      imagemin.optipng({
+        optimizationLevel: 3
+      }),
+
       imagemin.mozjpeg({
         quality: 75,
-        progressive: true           //{progressive: true} - покажет сразу все фото, но мЫло.
+        progressive: true
       }),
-      imagemin.svgo()               // оптимизируем svg
+      imagemin.svgo()
     ]))
-    .pipe(gulp.dest("build/img"));  // Результат кладем в соответствующую папку
+    .pipe(gulp.dest("build/img"));
 });
 
-// Задача сборки svg-спрайтов
-// SVG-спрайт ранее собрал вручную, но задачу напишу, запускать не буду
 gulp.task("sprite", function () {
   return gulp
-    .src(["source/img/{icon-,logo-pink-}*.svg", "source/img/htmlacademy.svg"]) // Ищу нужные svg
+    .src(["source/img/{icon-,logo-pink-}*.svg", "source/img/htmlacademy.svg"])
     .pipe(svgstore({
-      inlineSvg: true // Удаляем комментарии и иное в svg-коде
+      inlineSvg: true
     }))
-    .pipe(rename("sprite-auto.svg"))// Т.к. собранный ранее вручную спрайт положен в файл sprite.svg,
-                                    // то этот спрайт положится в файл sprite-auto.svg
-    .pipe(gulp.dest("source/img")); // Эту задачу буду запускать при необходимости вручную, поэтому
-                                    // итоговый спрайт кладу в папку "source/img".
+    .pipe(rename("sprite-auto.svg"))
+
+    .pipe(gulp.dest("source/img"));
+
 });
 
-// Задача, описывающая работу сервера
 gulp.task("server", function () {
-  server.init({        // Инициализация сервера
-    server: "build/", // Папка, в которой лежат папки и файлы проекта-сайта
+  server.init({
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
-  // Сервер, следи в указанной папке за файлами, указанного типа,
-  // и в случае изменений превращай их в css и сам перезапустись
-  gulp.watch("source/less/**/*.less", gulp.series("css"), server.reload);
+  gulp.watch("source/less/**/*.less", /*gulp.series("css"),*/ server.reload);
 
-  // Сервер, следи в указанной папке за файлами-html, и, в случае изменений, сам перезагрузись
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
-// Команда "build" Компилирует проект в папку "build"
-gulp.task("build", gulp.series("clear", "copy", "images", "css", "uglify", "html"));
+gulp.task("build", gulp.series("clear", "copy", "images", "css", "css2", "uglify", "html", "clean"));
 
-// Командой start запускаем сначала задачу "css", "html" и потом "server"
 gulp.task("start", gulp.series("build", "server"));
